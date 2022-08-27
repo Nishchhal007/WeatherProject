d7cb2510078a1bbeaff9c30e76f500199cee2eee
Nishchhal <nishchhal901@gmail.com>
2022-08-27 15:39:48 +0530

    Temperature & its details added

--- app.js
+++ app.js
@@ -5,21 +5,35 @@ const app = express();
 
 app.get("/",function (req,res) {
     
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Sikar&appid=4c3c6afd85f94ebf17f5d255f3211dcb&units=metric";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=sikar&appid=4c3c6afd85f94ebf17f5d255f3211dcb&units=metric";
 
     https.get(url,function (response) {
+        //Status Code
         console.log(response.statusCode);
 
         response.on("data",function (data) {
-           const weatherData= JSON.parse(data);
-           console.log(weatherData.weather[0].description);
-           
-        })
 
-    })
+           const weatherData= JSON.parse(data);
+           var temp = weatherData.main.temp;
+           var description = weatherData.weather[0].description.charAt(0).toUpperCase()+weatherData.weather[0].description.slice(1);
+           var icon = weatherData.weather[0].icon;
+           var iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+           
+
+           res.send(` <div style="display: flex; flex-direction: row">
+    <img src="${iconURL}" alt="" />
+    <div style="margin-left: 2%;">
+      <p id="temp" style="font-weight: 600;font-size: 2.3rem;">Temprature : ${temp}°C</p>
+      <p id="desc" style="font-weight: 550;font-size: 1.3rem;">The Weather is Currently ${description}</p>
+    </div>
+  </div>`); 
+
+           
+    });
+
+    });
     
-    
-    res.send("Server setup finally");
+    // res.send("Server setup finally");
 })
 
 

172c8dec1d65d387708da10004f738ad43382417
Nishchhal <nishchhal901@gmail.com>
2022-08-27 14:22:32 +0530

    get request method updated

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const https = require('https');
 
 const app = express();
 
 app.get("/",function (req,res) {
-    res.send("Server setup finally")
+    
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Sikar&appid=4c3c6afd85f94ebf17f5d255f3211dcb&units=metric";
+
+    https.get(url,function (response) {
+        console.log(response.statusCode);
+
+        response.on("data",function (data) {
+           const weatherData= JSON.parse(data);
+           console.log(weatherData.weather[0].description);
+           
+        })
+
+    })
+    
+    
+    res.send("Server setup finally");
 })
 
 
